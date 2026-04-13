8315a0c102bd76fe06205c4e11393ad52dc4c899
PushkarJatla <pushkarjatla02@gmail.com>
2026-04-13 13:38:34 +0530

    feat: add Redis integration for rate limiting and caching using cache-aside pattern to enhance performance and API protection

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 const authService = require("../services/auth.service");
 const asyncHandler = require("../utils/asyncHandler");
+const redis = require("../config/redis");
+const prisma = require("../config/prisma");
 
 const register = asyncHandler(async (req, res) => {
     const user = await authService.register(req.body);
@@ -26,8 +28,48 @@ const verifyToken = (req, res) => {
     });
 };
 
+const getMe = asyncHandler(async (req, res) => {
+    const userId = req.user.userId;
+
+    const cacheKey = `user:${userId}`;
+
+    // 🔥 1. Check cache
+    const cachedUser = await redis.get(cacheKey);
+
+    if (cachedUser) {
+        console.log("⚡ Cache HIT");
+
+        return res.status(200).json({
+            source: "cache",
+            user: JSON.parse(cachedUser),
+        });
+    }
+
+    console.log("🐢 Cache MISS");
+
+    // 🔥 2. Fetch from DB
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+        select: { id: true, email: true },
+    });
+
+    // 🔥 Optional: handle edge case
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+    // 🔥 3. Store in cache (TTL: 60 sec)
+    await redis.setex(cacheKey, 60, JSON.stringify(user));
+
+    res.status(200).json({
+        source: "db",
+        user,
+    });
+});
+
 module.exports = {
     register,
     login,
     verifyToken,
+    getMe,
 };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,10 +5,24 @@ const authController = require("../controllers/auth.controller");
 const authMiddleware = require("../middlewares/auth.middleware");
 const validate = require("../middlewares/validate.middleware");
 const { registerSchema, loginSchema } = require("../validations/auth.validation");
+const redisRateLimiter = require("../middlewares/redisRateLimiter.middleware");
 
-router.post("/register", validate(registerSchema), authController.register);
-router.post("/login", validate(loginSchema), authController.login);
+
+router.post(
+    "/login",
+    redisRateLimiter,
+    validate(loginSchema),
+    authController.login
+);
+
+router.post(
+    "/register",
+    redisRateLimiter,
+    validate(registerSchema),
+    authController.register
+);
 router.get("/verify", authMiddleware, authController.verifyToken);
+router.get("/me", authMiddleware, authController.getMe);
 
 
 module.exports = router;
