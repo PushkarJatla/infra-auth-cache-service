28aa846b0e5fe60b980b04faff8c544e9138d00a
PushkarJatla <pushkarjatla02@gmail.com>
2026-04-17 14:34:13 +0530

    updated cors

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ const authRoutes = require("./routes/auth.routes");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(helmet());
 app.use(morgan("dev"));
 
